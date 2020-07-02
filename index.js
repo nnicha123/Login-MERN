8341b7169cfdd62983296c3fbcb842179e97e0f5
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-02 13:26:41 +0700

    then connected to mongoDB

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+// connect to mongoDB
+mongoose.connect('mongodb://localhost/login',{ useUnifiedTopology: true, 
+useNewUrlParser: true },() => console.log('Connected to DB'))
+
 app.get('/',(req,res) => res.send('Hello'))
 app.listen(5000,() => console.log('Listening on port 5000'))
